083dbf2064c0278e738b442184706e6f4de22a2f
Tom Randles <randles.tom@gmail.com>
2021-07-23 11:05:03 +0100

    C# 8 Async Streams and Static Local Function examples

---
+++ AsyncStreams_StaticLocalFunctions/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AsyncStreams_StaticLocalFunctions
+{
+    class Program
+    {
+        static async void Main(string[] args)
+        {
+            // https://www.c-sharpcorner.com/learn/learn-c-sharp-80/async-streams-static-local-functions
+            Console.WriteLine("C# 8 Async Streams and Static Local Functions");
+            await ExecuteAsyncStream();
+
+            ExecuteLocalFunction();
+        }
+
+        // Async streams
+        public static async Task ExecuteAsyncStream()
+        {
+            await foreach (var number in GenerateSequence())
+            {
+                Console.WriteLine($"The square of number {number.Number} is: {number.Square}");
+            }
+        }
+        public static async IAsyncEnumerable<SquareNumber> GenerateSequence()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                await Task.Delay(200);
+                yield return new SquareNumber
+                {
+                    Number = i,
+                    Square = i * i
+                };
+            }
+        }
+
+        // Static Local Functions
+        public static void ExecuteLocalFunction()
+        {
+            Console.WriteLine(InstanceLocalMethod());
+            Console.WriteLine(StaticLocalMethod());
+        }
+        static int InstanceLocalMethod()
+        {
+            int x = 7;
+            return Square();
+            int Square() => x * x;
+        }
+        static int StaticLocalMethod()
+        {
+            int x = 7;
+            return Square(x);
+            static int Square(int num) => num * num;
+        }
+    }
+
+    public class SquareNumber
+    {
+        public int Number { get; set; }
+        public int Square { get; set; }
+    }
+}
