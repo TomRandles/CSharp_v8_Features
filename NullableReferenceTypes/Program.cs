c6872ead2f493d8fc28268275efdbce62e2b46da
Tom Randles <randles.tom@gmail.com>
2021-07-23 10:30:24 +0100

    Add C# 8 Nullable Reference Types examples

---
+++ NullableReferenceTypes/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace NullableReferenceTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C# v8 Nullable Reference Types");
+
+            ExecuteNonNullableReferenceType();
+            ExecuteNullableReferenceType();
+            ExecuteNullableReferenceType2();
+        }
+
+        public static void ExecuteNonNullableReferenceType()
+        {
+            #nullable enable
+            string name = null;
+            var myName = name.ToString();
+            #nullable restore
+        }
+
+        public static void ExecuteNullableReferenceType()
+        {
+            #nullable enable
+            string? name = null;
+            var myName = name.ToString();
+            #nullable restore
+        }
+        public static void ExecuteNullableReferenceType2()
+        {
+            #nullable enable
+            string? name = null;
+            var myName = name!.ToString(); // Null Forgiving Operator   
+            #nullable restore
+        }
+
+
+    }
+}
